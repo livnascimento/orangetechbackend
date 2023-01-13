0866220825926b1d3ba3bb18af788a860c836285
Lívia Nascimento <liviapnd@gmail.com>
2023-01-13 16:04:02 -0300

    ordem inversa

---
+++ orangetech/src/edu/livia/curso/repeticaoearrays/arrays/OrdemInversa.java
@@ -0,0 +1,31 @@
+package edu.livia.curso.repeticaoearrays.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Crie um vetor de 6 números inteiros
+e mostre-os na ordem inversa.
+*/
+
+public class OrdemInversa {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] numeros = new int[6];
+		
+		for (int i = 0 ; i < numeros.length ; i++) 
+			numeros[i] = sc.nextInt();
+		
+		Arrays.sort(numeros);
+		
+		for (int i = (numeros.length - 1) ; i >= 0 ; i--)
+			System.out.println(numeros[i]);
+		
+		sc.close();
+		
+	}
+
+}
