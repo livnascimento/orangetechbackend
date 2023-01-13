77455c0ce1032df91cc5901f27853a636b3baab3
Lívia Nascimento <liviapnd@gmail.com>
2023-01-13 16:23:22 -0300

    consoantes com stream e match

---
+++ orangetech/src/edu/livia/curso/repeticaoearrays/arrays/Consoantes.java
@@ -0,0 +1,41 @@
+package edu.livia.curso.repeticaoearrays.arrays;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+/*
+Faça um Programa que leia um vetor de 6 caracteres,
+e diga quantas consoantes foram lidas.
+Imprima as consoantes.
+*/
+
+public class Consoantes {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String[] letras = new String[6];
+		String[] vogais = {"a", "e", "i", "o", "u"};
+		int consoantes = 0;
+		
+		for (int i = 0; i < letras.length ; i ++) {
+			letras[i] = sc.next();
+		}
+		
+		for (String string : letras) {
+			
+			boolean consoante = Arrays.stream(vogais).noneMatch(string::equals);
+			
+			if(consoante) {
+				consoantes++;
+			};
+		}
+		
+		System.out.println(consoantes);
+		
+		sc.close();
+		
+	}
+
+}
