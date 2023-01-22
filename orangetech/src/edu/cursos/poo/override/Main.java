63e6d26d9ac04e5bfc62bc1ea273abf504a5ef23
Lívia Nascimento <liviapnd@gmail.com>
2023-01-22 08:27:58 -0300

    correção

--- orangetech/src/edu/cursos/poo/override/Main.java
+++ orangetech/src/edu/cursos/poo/override/Main.java
@@ -6,9 +6,16 @@ public class Main {
 		
 		SuperClasse[] classes = new SuperClasse[] {new SubClasse1(), new SubClasse2(), new SuperClasse()};
 		
+		// polimorfismo
+		
 		for (SuperClasse classe : classes) {
 			classe.metodo1();
 		}
+		
+		// sobreescrita "pura"
+		
+		SubClasse2 subclasse2 = new SubClasse2();
+		subclasse2.metodo2();
 		
 	}
 
