c320f12246dc8989d7a3020cba1e102baf8f1cab
Lívia Nascimento <liviapnd@gmail.com>
2023-01-27 13:53:19 -0300

    exercício interfaces

---
+++ orangetech/src/edu/cursos/poo/interfacecalculadora/Main.java
@@ -0,0 +1,31 @@
+package edu.cursos.poo.interfacecalculadora;
+
+public class Main implements Operacoes{
+
+	@Override
+	public void soma() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void subtracao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void multiplicacao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void divisao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
---
+++ orangetech/src/edu/cursos/poo/interfacecalculadora/Operacoes.java
@@ -0,0 +1,13 @@
+package edu.cursos.poo.interfacecalculadora;
+
+public interface Operacoes {
+	
+	void soma();
+	
+	void subtracao();
+	
+	void multiplicacao();
+	
+	void divisao();
+	
+}
